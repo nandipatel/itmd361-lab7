facabcdd3bb5a0726f60c6023daeb7d30440bcb7
nandipatel <60127512+nandipatel@users.noreply.github.com>
2020-04-21 18:48:49 -0500

    adding variables

--- js/script.js
+++ js/script.js
@@ -1,11 +1,9 @@
 function init(){
-//add your javascrip between these two lines of code
  
-
-
-
-
-
-
+ var entry = document.getElementById('entryinput');
+ var enbutton = document.getElementById('entrybutton');
+ var txoutput = document.getElementById('textoutput');
+ var name = document.getElementByClassName('center') [1];
+ 
 
 window.addEventListener('load', init);
