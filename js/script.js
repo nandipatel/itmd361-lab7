d6a3769d64cd425f84794f654f8abf00b99145d7
nandipatel <60127512+nandipatel@users.noreply.github.com>
2020-04-21 21:04:03 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,13 +3,14 @@ function init(){
  var entry = document.getElementById('entryinput');
  var enbutton = document.getElementById('entrybutton');
  var txoutput = document.getElementById('textoutput');
- var name = document.getElementByClassName('center') [1];
 
  function alert(){
-  alert(name.textContent + ': ' + entry.value);}
+  alert('Nandi Patel: ' + entry.value);
+ }
  
  function output(){
-  txoutput.innerHTML = entry.value;}
+  txoutput.innerHTML = entry.value;
+ }
 
  enbutton.addEventListener('click', alert);
  enbutton.addEventListener('click', output);
