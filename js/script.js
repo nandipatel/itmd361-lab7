6b233a2a9aa9183aaa8a5bec1919d1e983766dc0
nandipatel <60127512+nandipatel@users.noreply.github.com>
2020-04-21 20:58:46 -0500

    fixing errors
     lowercase to capital & double to single quotes

--- js/script.js
+++ js/script.js
@@ -6,13 +6,13 @@ function init(){
  var name = document.getElementByClassName('center') [1];
 
  function alert(){
-  alert(name.textcontent + ': ' + entry.value);}
+  alert(name.textContent + ': ' + entry.value);}
  
  function output(){
   txoutput.innerHTML = entry.value;}
 
- enbutton.addEventListener("click", alert);
- enbutton.addEventListener("click", output);
+ enbutton.addEventListener('click', alert);
+ enbutton.addEventListener('click', output);
  
 }
 
